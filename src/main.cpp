b2029af53fef745630c327dac03a6a148027a081
Wiedjaja Atmadja <swiedjaja@binus.ac.id>
2024-12-06 18:57:23 +0700

    Add thingsboard

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
+/*
+Test Thingsboard device: 
+  curl -v -X POST http://demo.thingsboard.io/api/v1/tic0iv4wh04e1bqwpxn3/telemetry --header Content-Type:application/json --data "{temperature:25}"
+
+*/
 #include <Arduino.h>
 #include <WiFi.h>
+#include "ThingsBoard.h"
 #include "DHTesp.h"
 
 #define LED_GREEN 4
 #define LED_RED 5
 #define DHT_PIN 6
 #define SWITCH_PIN 9
+
+#define THINGSBOARD_SERVER "demo.thingsboard.io"
+#define THINGSBOARD_ACCESS_TOKEN "tic0iv4wh04e1bqwpxn3"
 DHTesp dht;
+WiFiClient espClient;
+ThingsBoard tb(espClient);
 
 void setup() {
   // put your setup code here, to run once:
@@ -27,16 +38,28 @@ void setup() {
   Serial.print("System connected with IP address: ");
   Serial.println(WiFi.localIP());
   Serial.printf("RSSI: %d\n", WiFi.RSSI());
+
+  if (tb.connect(THINGSBOARD_SERVER, THINGSBOARD_ACCESS_TOKEN)) {
+    Serial.println("Connected to ThingsBoard");
+  } else {
+    Serial.println("Failed to connect to ThingsBoard");
+  }
 }
 
 long nCurrentMillis = 0;
 void loop() {
-  if (millis() - nCurrentMillis > 1000)
+  if (millis() - nCurrentMillis > 3000)
   {
     nCurrentMillis = millis();
     float fTemperature = dht.getTemperature();
     float fHumidity = dht.getHumidity();
     Serial.printf("Temperature: %.2f\r\n", fTemperature);
     Serial.printf("Humidity: %.2f\r\n", fHumidity);
+    if (tb.connected())
+    {
+      tb.sendTelemetryFloat("temperature", fTemperature);
+      tb.sendTelemetryFloat("humidity", fHumidity);
+    }
   }
+  tb.loop();
 }
