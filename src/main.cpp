343b1f8321729d72462729bfeafb517b3f82d05d
Wiedjaja Atmadja <swiedjaja@binus.ac.id>
2024-12-06 18:04:23 +0700

    add DHT22

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include "DHTesp.h"
 
 #define LED_GREEN 4
 #define LED_RED 5
 #define DHT_PIN 6
 #define SWITCH_PIN 9
+DHTesp dht;
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,6 +14,7 @@ void setup() {
   pinMode(LED_GREEN, OUTPUT);
   pinMode(LED_RED, OUTPUT);
   pinMode(SWITCH_PIN, INPUT_PULLUP);
+  dht.setup(DHT_PIN, DHTesp::DHT22);
   Serial.println("Booting...");
   WiFi.mode(WIFI_STA);
   WiFi.begin("Wokwi-GUEST", "", 6);
@@ -26,6 +29,14 @@ void setup() {
   Serial.printf("RSSI: %d\n", WiFi.RSSI());
 }
 
+long nCurrentMillis = 0;
 void loop() {
-
+  if (millis() - nCurrentMillis > 1000)
+  {
+    nCurrentMillis = millis();
+    float fTemperature = dht.getTemperature();
+    float fHumidity = dht.getHumidity();
+    Serial.printf("Temperature: %.2f\r\n", fTemperature);
+    Serial.printf("Humidity: %.2f\r\n", fHumidity);
+  }
 }
